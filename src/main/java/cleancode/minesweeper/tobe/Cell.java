e28c54a4585362725ca32459f2bee18f51e68669
boki <lsb530@naver.com>
2024-10-08 13:54:26 +0900

    객체지향 설계하기(2)

---
+++ src/main/java/cleancode/minesweeper/tobe/Cell.java
@@ -0,0 +1,83 @@
+package cleancode.minesweeper.tobe;
+
+public class Cell {
+
+    private static final String FLAG_SIGN = "⚑";
+    private static final String LAND_MINE_SIGN = "☼";
+    private static final String UNCHECKED_SIGN = "□";
+    private static final String EMPTY_SIGN = "■";
+
+    private int nearbyLandMinCount;
+    private boolean isLandMine;
+    private boolean isFlagged;
+    private boolean isOpened;
+
+    private Cell(
+        int nearbyLandMinCount,
+        boolean isLandMine, boolean isFlagged, boolean isOpened
+    ) {
+        this.nearbyLandMinCount = nearbyLandMinCount;
+        this.isLandMine = isLandMine;
+        this.isFlagged = isFlagged;
+        this.isOpened = isOpened;
+    }
+
+    public static Cell of(int nearbyLandMinCount, boolean isLandMine, boolean isFlagged, boolean isOpened) {
+        return new Cell(nearbyLandMinCount, isLandMine, isFlagged, isOpened);
+    }
+
+    public static Cell create() {
+        return of(0, false, false, false);
+    }
+
+    public void turnOnLandMine() {
+        this.isLandMine = true;
+    }
+
+    public void updateNearbyLandMindCount(int nearbyLandMinCount) {
+        this.nearbyLandMinCount = nearbyLandMinCount;
+    }
+
+    public void flag() {
+        this.isFlagged = true;
+    }
+
+    public void open() {
+        this.isOpened = true;
+    }
+
+    public boolean isChecked() {
+        return isFlagged || isOpened;
+    }
+
+    public boolean isLandMine() {
+        return isLandMine;
+    }
+
+    public boolean isOpened() {
+        return isOpened;
+    }
+
+    public boolean hasLandMineCount() {
+        return this.nearbyLandMinCount != 0;
+    }
+
+    public String getSign() {
+        if (isOpened) {
+            if (isLandMine) {
+                return LAND_MINE_SIGN;
+            }
+            if (hasLandMineCount()) {
+                return String.valueOf(nearbyLandMinCount);
+            }
+            return EMPTY_SIGN;
+        }
+
+        if (isFlagged) {
+            return FLAG_SIGN;
+        }
+
+        return UNCHECKED_SIGN;
+    }
+
+}
