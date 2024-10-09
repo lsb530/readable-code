b39797d2a74d53ebfbf51863e8533010013ee56a
boki <lsb530@naver.com>
2024-10-09 15:57:46 +0900

    DIP 적용 1차

---
+++ src/main/java/cleancode/minesweeper/tobe/io/OutputHandler.java
@@ -0,0 +1,16 @@
+package cleancode.minesweeper.tobe.io;
+
+import cleancode.minesweeper.tobe.GameBoard;
+import cleancode.minesweeper.tobe.GameException;
+
+public interface OutputHandler {
+
+    void showGameStartComments();
+    void showBoard(GameBoard gameBoard);
+    void printGameWinningComment();
+    void printGameLosingComment();
+    void printCommentForSelectingCell();
+    void printCommentForUserAction();
+    void printExceptionMessage(GameException e);
+    void printSimpleMessage(String message);
+}
