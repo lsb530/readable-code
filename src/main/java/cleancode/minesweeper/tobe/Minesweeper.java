8963164bdc85404cc13879072fff4c71a31b6edd
boki <lsb530@naver.com>
2024-10-09 14:20:15 +0900

    OCP 적용

--- src/main/java/cleancode/minesweeper/tobe/Minesweeper.java
+++ src/main/java/cleancode/minesweeper/tobe/Minesweeper.java
@@ -1,19 +1,21 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
-import cleancode.studycafe.tobe.GameBoard;
 
 public class Minesweeper {
 
-    public static final int BOARD_ROW_SIZE = 8;
-    public static final int BOARD_COL_SIZE = 10;
-
-    private final GameBoard gameBoard = new GameBoard(BOARD_ROW_SIZE, BOARD_COL_SIZE);
+    private final GameBoard gameBoard;
+    private final BoardIndexConverter boardIndexConverter = new BoardIndexConverter();
     private final ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
     private final ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
 
     private int gameStatus = 0; // 0: 게임 중, 1: 승리, -1: 패배
+
+    public Minesweeper(GameLevel gameLevel) {
+        gameBoard = new GameBoard(gameLevel);
+    }
 
     public void run() {
         consoleOutputHandler.showGameStartComments();
@@ -44,8 +46,8 @@ public class Minesweeper {
     }
 
     private void actOnCell(String cellInput, String userActionInput) {
-        int selectedColIndex = getSelectedColIndex(cellInput);
-        int selectedRowIndex = getSelectedRowIndex(cellInput);
+        int selectedColIndex = boardIndexConverter.getSelectedColIndex(cellInput, gameBoard.getColSize());
+        int selectedRowIndex = boardIndexConverter.getSelectedRowIndex(cellInput, gameBoard.getRowSize());
 
         if (doesUserChooseToPlanFlag(userActionInput)) {
             gameBoard.flag(selectedRowIndex, selectedColIndex);
@@ -55,7 +57,7 @@ public class Minesweeper {
 
         if (doesUserChooseToOpenCell(userActionInput)) {
             if (gameBoard.isLandMineCell(selectedRowIndex, selectedColIndex)) {
-                gameBoard.open(selectedRowIndex,selectedColIndex);
+                gameBoard.open(selectedRowIndex, selectedColIndex);
                 changeGameStatusToLose();
                 return;
             }
@@ -77,16 +79,6 @@ public class Minesweeper {
 
     private boolean doesUserChooseToPlanFlag(String userActionInput) {
         return userActionInput.equals("2");
-    }
-
-    private int getSelectedRowIndex(String cellInput) {
-        char cellInputRow = cellInput.charAt(1);
-        return convertRowFrom(cellInputRow);
-    }
-
-    private int getSelectedColIndex(String cellInput) {
-        char cellInputCol = cellInput.charAt(0);
-        return convertColFrom(cellInputCol);
     }
 
     private String getUserActionInputFromUser() {
@@ -115,30 +107,6 @@ public class Minesweeper {
 
     private void changeGameStatusToWin() {
         gameStatus = 1;
-    }
-
-    private int convertRowFrom(char cellInputRow) {
-        int rowIndex = Character.getNumericValue(cellInputRow) - 1;
-        if (rowIndex >= BOARD_ROW_SIZE) {
-            throw new GameException("잘못된 입력입니다.");
-        }
-        return rowIndex;
-    }
-
-    private int convertColFrom(char cellInputCol) {
-        return switch (cellInputCol) {
-            case 'a' -> 0;
-            case 'b' -> 1;
-            case 'c' -> 2;
-            case 'd' -> 3;
-            case 'e' -> 4;
-            case 'f' -> 5;
-            case 'g' -> 6;
-            case 'h' -> 7;
-            case 'i' -> 8;
-            case 'j' -> 9;
-            default -> throw new GameException("잘못된 입력입니다.");
-        };
     }
 
 }
---
+++ src/main/java/cleancode/minesweeper/tobe/gamelevel/Middle.java
@@ -0,0 +1,23 @@
+package cleancode.minesweeper.tobe.gamelevel;
+
+public class Middle implements GameLevel {
+
+    private static final int ROWS = 14;
+    private static final int COLS = 18;
+    private static final int LANDMINES = 40;
+
+    @Override
+    public int getRowSize() {
+        return ROWS;
+    }
+
+    @Override
+    public int getColSize() {
+        return COLS;
+    }
+
+    @Override
+    public int getLandMineCount() {
+        return LANDMINES;
+    }
+}
