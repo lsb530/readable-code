c1f8d85f60c97da10c225d1b817b5148349c5547
boki <lsb530@naver.com>
2024-10-09 15:10:06 +0900

    ISP 위반

---
+++ src/main/java/cleancode/minesweeper/tobe/AnotherGame.java
@@ -0,0 +1,14 @@
+package cleancode.minesweeper.tobe;
+
+public class AnotherGame implements Game {
+
+    @Override
+    public void initialize() {
+        // ...필요없는데..?
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
