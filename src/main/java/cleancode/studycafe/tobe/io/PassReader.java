54a4e83bd9b615fffa9f5a55298de27d23d8bb35
boki <lsb530@naver.com>
2024-10-15 13:20:01 +0900

    리팩토링(3) - 잘못된 추상화(데이터를 어디로부터 어떻게 가져올 것인가에만 집중)

---
+++ src/main/java/cleancode/studycafe/tobe/io/PassReader.java
@@ -0,0 +1,12 @@
+package cleancode.studycafe.tobe.io;
+
+import cleancode.studycafe.tobe.model.pass.StudyCafeSeatPasses;
+import cleancode.studycafe.tobe.model.pass.locker.StudyCafeLockerPasses;
+
+public interface PassReader {
+
+    StudyCafeSeatPasses readStudyCafePasses();
+
+    StudyCafeLockerPasses readLockerPasses();
+
+}
